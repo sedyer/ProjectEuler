5f8a1060c2619878988d54e995d2e3ff1dd91508
Shane Dyer <Shane.Dyer@zirmed.com>
2017-08-11 10:24:21 -0400

    initial project

---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static int SumOfFactorsFinder(List<int> factors, int stopValue)
+        {
+            int sum = 0;
+
+            return sum;
+        }
+    }
+}
