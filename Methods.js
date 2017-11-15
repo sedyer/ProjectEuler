bb6ca400b34dc9dcf720174693850e65b1badae6
Shane Dyer <Shane.Dyer@zirmed.com>
2017-11-15 14:22:27 -0500

    add some semicolons to make jshint happy

--- Methods.js
+++ Methods.js
@@ -2,42 +2,42 @@ function isPrime(num)
 {
     for (var d = 2; d < num; d++)
     {
-        if (num % d === 0) return false
+        if (num % d === 0) return false;
     }
 
-    return true
+    return true;
 }
 
 function getNthPrime(num) 
 {
-    var primesFound = 1
-    var currentNumber = 3
+    var primesFound = 1;
+    var currentNumber = 3;
 
     while (primesFound != num)
     {
-        if (isPrime(currentNumber)) primesFound++
-        if (primesFound === num)    return currentNumber
-        currentNumber += 2
+        if (isPrime(currentNumber)) primesFound++;
+        if (primesFound === num)    return currentNumber;
+        currentNumber += 2;
     }
 }
 
 function getLargestSubstringProduct(string, length) 
 {
-    var largestProduct = 0
+    var largestProduct = 0;
 
     for (var i = 0; i <= string.length - length; i++) 
     {
-        var substring = string.substring(i, i + length).split("")
+        var substring = string.substring(i, i + length).split("");
 
-        var product = substring.reduce((x,y) => parseInt(x) * parseInt(y))
+        var product = substring.reduce((x,y) => parseInt(x) * parseInt(y));
 
         if (product > largestProduct)
         {
-            largestProduct = product
+            largestProduct = product;
         }
     }
 
-    return largestProduct
+    return largestProduct;
 }
 
-console.log("test")
+console.log("test");
