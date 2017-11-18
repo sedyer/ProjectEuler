1345b4ff631dd78270eb7ae674da18d16eba76a9
Shane Dyer <aleph.null.00000@gmail.com>
2017-11-18 12:18:16 -0500

    try out new run/debug stuff

--- coffee/cMethods.js
+++ coffee/cMethods.js
@@ -51,10 +51,10 @@
 
   comparator = function(x, y) {
     if (x[0] === y[0]) {
-      0;
+      return 0;
     }
     if (x[0] < y[0]) {
-      -1;
+      return -1;
     }
     return 1;
   };
--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 var methods = require('../coffee/cMethods');
 
-var result = methods.findPythagoreanTripletWithGivenSum(12);
+var result = methods.findPythagoreanTripletWithGivenSum(1000);
+
+console.log(result);
